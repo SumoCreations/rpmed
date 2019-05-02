29bad21e7459622eb354db0a0c678f2635e85cf6
Jim Jeffers <jim@sumocreations.com>
2019-05-02 15:17:08 -0700

    Exposing new distributor and symptom schemas on master schema.

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -1,9 +1,11 @@
 import { makeExecutableSchema, mergeSchemas } from "apollo-server-lambda"
 import * as customers from "./customers"
+import * as distributors from "./distributors"
 import * as products from "./products"
+import * as productSymptoms from "./productSymptoms"
 import * as users from "./users"
 
-export const schemas = [customers, products, users]
+export const schemas = [customers, distributors, products, productSymptoms, users]
 export const schema = mergeSchemas({
   schemas: schemas.map(({ typeDefs, resolvers }) =>
     makeExecutableSchema({ typeDefs, resolvers })
