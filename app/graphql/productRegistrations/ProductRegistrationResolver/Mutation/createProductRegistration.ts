b3db881d1deab29050559f45b740ea1f17500d34
Jim Jeffers <jim@sumocreations.com>
2019-05-10 14:35:23 -0700

    Create registration utilizes shared validation module

--- app/graphql/productRegistrations/ProductRegistrationResolver/Mutation/createProductRegistration.ts
+++ app/graphql/productRegistrations/ProductRegistrationResolver/Mutation/createProductRegistration.ts
@@ -1,71 +1,32 @@
-import * as Validation from "rpmed-validation-schema"
-import { v4 as uuid } from "uuid"
-import { Customer, IProductRegistrationInput, ModelNumber, ProductRegistration } from "../../../../models"
-import * as E from "../productRegistrationErrors"
+import { Customer, IProductRegistrationInput, ProductRegistration } from "../../../../models"
 import { IProductRegistrationMutationOutput } from "./productRegistrationMutationTypes"
+import { validateRegistrationInput } from "./validateRegistrationInput"
 
 export const createProductRegistration = async (
   _: any,
   { productRegistrationInput }: { productRegistrationInput: IProductRegistrationInput }
 ): Promise<IProductRegistrationMutationOutput> => {
-  try {
-    await Validation.ProductRegistration.Default.validate(productRegistrationInput, { abortEarly: false })
-  } catch (e) {
-    return { errors: Validation.formatError(e), success: false }
+  const { errorResponse, input, customer } = await validateRegistrationInput(productRegistrationInput)
+  if (errorResponse) {
+    return errorResponse
   }
-
-  const relatedCustomer = await Customer.find(productRegistrationInput.customerId)
-  if (!relatedCustomer) {
-    return {
-      errors: [E.ErrorProductRegistrationCustomerDoesNotExist],
-      success: false
-    }
-  }
-
-  const relatedModel = await ModelNumber.find(productRegistrationInput.modelNumber)
-  if (!relatedModel) {
-    return {
-      errors: [E.ErrorProductRegistrationModelNumberDoesNotExist],
-      success: false
-    }
-  }
-  const hasSerial = productRegistrationInput.serial && productRegistrationInput.serial.length > 0
-  if (relatedModel.lotted && !hasSerial) {
-    return {
-      errors: [E.ErrorProductRegistrationWithSerialCannotBeBlank],
-      success: false
-    }
-  }
-  if (!relatedModel.lotted && hasSerial) {
-    return {
-      errors: [E.ErrorProductRegistrationWithSerialMustBeBlank],
-      success: false
-    }
-  }
-
-  const id = productRegistrationInput.serial || uuid()
-  const existingProductRegistration = await ProductRegistration.find(id)
-  if (existingProductRegistration) {
-    return {
-      errors: [E.ErrorProductRegistrationWithSerialAlreadyExists],
-      success: false
-    }
-  }
-
   try {
     const productRegistration = await ProductRegistration.create({
-      ...productRegistrationInput,
-      lotted: relatedModel.lotted,
-      productId: relatedModel.indexSortKey,
-      serial: id
+      ...input
     })
     return {
       productRegistration: {
         ...ProductRegistration.output(productRegistration),
-        customer: async () => Customer.output((await Customer.find(productRegistration.customerId)))
+        customer: async () => Customer.output(customer || (await Customer.find(productRegistration.customerId)))
       }, success: true
     }
   } catch (e) {
-    return { success: false, errors: [E.ErrorProductRegistrationCredentialsInvalid] }
+    return {
+      errors: [{
+        message: e.localizedMessage || `Could not create registration`,
+        path: "_"
+      }],
+      success: false,
+    }
   }
 }
