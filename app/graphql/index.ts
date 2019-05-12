28bb8d804720e0e7260c660f2cde50d38ddee284
Jim Jeffers <jim@sumocreations.com>
2019-05-12 13:16:47 -0700

    Revised file names and properly exposed RGA resolvers to the rest of the library.

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -4,9 +4,10 @@ import * as distributors from "./distributors"
 import * as productRegistrations from "./productRegistrations"
 import * as products from "./products"
 import * as productSymptoms from "./productSymptoms"
+import * as rgas from "./rgas"
 import * as users from "./users"
 
-export const schemas = [customers, distributors, products, productRegistrations, productSymptoms, users]
+export const schemas = [customers, distributors, products, productRegistrations, productSymptoms, users, rgas]
 export const schema = mergeSchemas({
   schemas: schemas.map(({ typeDefs, resolvers }) =>
     makeExecutableSchema({ typeDefs, resolvers })
--- app/graphql/rga/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./RGAResolvers"
-export * from "./RGASchema"
