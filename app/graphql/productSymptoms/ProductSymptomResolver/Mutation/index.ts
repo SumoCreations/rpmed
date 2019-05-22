b3175bdfb74a266d89ce2ade7646157fc474f4ba
Jim Jeffers <jim@sumocreations.com>
2019-05-22 13:32:48 -0700

    Implemented linkSymptomToModelMutation.

--- app/graphql/productSymptoms/ProductSymptomResolver/Mutation/index.ts
+++ app/graphql/productSymptoms/ProductSymptomResolver/Mutation/index.ts
@@ -1,3 +1,4 @@
 export * from "./createProductSymptom"
 export * from "./destroyProductSymptom"
 export * from "./updateProductSymptom"
+export * from "./linkSymptomToModel"
---
+++ app/graphql/productSymptoms/ProductSymptomResolver/Mutation/linkSymptomToModel.ts
@@ -0,0 +1,53 @@
+import { formatError, RequiredBoolean, RequiredString, validation } from "rpmed-validation-schema"
+import { addSymptomToModelNumber, ModelNumber, ProductSymptom, removeSymptomFromModelNumber } from "../../../../models"
+import { ErrorModelNumberIDDoesNotExist, ErrorProductSymptomAndModelNumberAssociationFailed, ErrorProductSymptomWithIDDoesNotExist } from "../productSymptomErrors"
+import { IProductSymptomMutationOutput } from "./productSymptomMutationTypes"
+
+interface ILinkSymptomMutationArguments { modelNumber: string, symptomId: string, linked: boolean }
+
+export const linkSymptomToModel = async (
+  _: any,
+  { modelNumber, symptomId, linked }: ILinkSymptomMutationArguments
+): Promise<IProductSymptomMutationOutput> => {
+  const argumentSchema = validation({
+    linked: RequiredBoolean(),
+    modelNumber: RequiredString(),
+    symptomId: RequiredString(),
+  })
+  try {
+    await argumentSchema.validate({ modelNumber, symptomId, linked }, { abortEarly: false })
+  } catch (e) {
+    return { errors: formatError(e), success: false }
+  }
+
+  const existingProductSymptom = await ProductSymptom.find(symptomId)
+  if (!existingProductSymptom) {
+    return {
+      errors: [ErrorProductSymptomWithIDDoesNotExist],
+      success: false
+    }
+  }
+
+  const existingModelNumber = await ModelNumber.find(modelNumber)
+  if (!existingModelNumber) {
+    return {
+      errors: [ErrorModelNumberIDDoesNotExist],
+      success: false
+    }
+  }
+
+  try {
+    if (linked) {
+      await addSymptomToModelNumber(symptomId, modelNumber)
+    } else {
+      await removeSymptomFromModelNumber(symptomId, modelNumber)
+    }
+    return {
+      success: true
+    }
+  } catch (e) {
+    // tslint:disable-next-line
+    console.log(e)
+    return { success: false, errors: [ErrorProductSymptomAndModelNumberAssociationFailed] }
+  }
+}
