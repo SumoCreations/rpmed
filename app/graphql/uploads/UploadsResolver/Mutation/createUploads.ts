4d467d58f945beb9486e6d38ca7f489e9a77a4c1
Jim Jeffers <jim@sumocreations.com>
2019-07-23 13:56:04 -0700

    Added upload generation schema.

---
+++ app/graphql/uploads/UploadsResolver/Mutation/createUploads.ts
@@ -0,0 +1,53 @@
+import { ErrorList } from "rpmed-validation-schema"
+import { getS3 } from "../../../../util"
+
+interface IUploadInput {
+  keys: string[]
+}
+
+interface IUpload {
+  id: string
+  bucket: string
+  url: string
+}
+
+export interface IUploadMutationOutput {
+  uploads?: IUpload[]
+  errors?: ErrorList
+  success: boolean
+}
+
+const s3 = getS3()
+
+const getUploadURL = (key: string): Promise<IUpload> => new Promise((resolve, reject) => {
+  const params = {
+    Bucket: 'rpmed-dev-uploads',
+    Key: key
+  }
+  s3.getSignedUrl('putObject', params, (err, data) => {
+    if (err) {
+      // tslint:disable-next-line
+      console.error('Presigning post data encountered an error', err)
+      reject(err)
+    } else {
+      resolve({
+        bucket: params.Bucket,
+        id: params.Key,
+        url: data,
+      })
+    }
+  })
+})
+
+export const createUploads = async (
+  _: any,
+  { uploadInput }: { uploadInput: IUploadInput }
+): Promise<IUploadMutationOutput> => {
+  try {
+    const { keys } = uploadInput
+    const uploads = await Promise.all(keys.map(async (key) => await getUploadURL(key)))
+    return { uploads, success: true }
+  } catch (e) {
+    return { success: false, errors: [] }
+  }
+}
