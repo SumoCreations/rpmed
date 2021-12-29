34a2f59b9a607309e047ee5d56980b3adfe46b39
Jim Jeffers <jim@sumocreations.com>
2021-12-28 22:06:47 -0800

    Fixed import for admin css.

--- packages/rpmed-admin/src/index.tsx
+++ packages/rpmed-admin/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
-import 'rpmed-ui/lib/rpmed-ui.scss'
+import 'rpmed-ui/lib/rpmed-ui.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
