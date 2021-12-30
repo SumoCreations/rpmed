7a3bc40cce3cc4c4c89a1558c1ee99c8702b9c87
Jim Jeffers <jim@sumocreations.com>
2021-12-29 20:48:40 -0800

    Fixed tailwind build process and removed tailwind package from admin app.

--- packages/rpmed-ui/tailwind.config.js
+++ packages/rpmed-ui/tailwind.config.js
@@ -7,12 +7,6 @@ module.exports = {
   ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    inset: {
-      0: 0,
-      auto: 'auto',
-      '1/2': '50%',
-      100: '100%',
-    },
     extend: {
       colors: {
         body: '#3b3a3d',
@@ -24,6 +18,12 @@ module.exports = {
         primary: '#4b5472',
         secondary: '#e6e5e8',
       },
+      // inset: {
+      //   0: 0,
+      //   auto: 'auto',
+      //   '1/2': '50%',
+      //   100: '100%',
+      // },
     },
   },
   variants: {
